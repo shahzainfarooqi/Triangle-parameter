7efde745830e8167251902620c2874b640d0b74b
shahzainfarooqi <142692117+shahzainfarooqi@users.noreply.github.com>
2025-04-27 18:12:30 +0500

    Add files via upload

---
+++ tri.py
@@ -0,0 +1,10 @@
+# Prompting the user for the lengths of the sides
+side1 = float(input("What is the length of side 1? "))
+side2 = float(input("What is the length of side 2? "))
+side3 = float(input("What is the length of side 3? "))
+
+# Calculating the perimeter
+perimeter = side1 + side2 + side3
+
+# Printing the perimeter
+print("The perimeter of the triangle is", perimeter)
